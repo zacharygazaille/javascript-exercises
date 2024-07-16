78685665bc131c8f5d63aef5afe4edfbea2df693
Zachary Gazaille <zacharygazaille@gmail.com>
2024-07-16 19:57:39 -0400

    Complete exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(numsArray, ...args) {
+    const tempArray = [];
+    numsArray.forEach((item) => {
+        if (!args.includes(item)) {
+            tempArray.push(item);
+        }
+    });
+    return tempArray;
 };
 
 // Do not edit below this line
