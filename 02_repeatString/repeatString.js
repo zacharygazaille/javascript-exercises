0e088734da26b897e4b1a6654b7568052349392c
Zachary Gazaille <zacharygazaille@gmail.com>
2024-07-16 19:25:46 -0400

    Complete exercise 02 and 03

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0) {
+        return ("ERROR");
+    }
+    let word = "";
+    for (let i = 0; i < num; i++) {
+        word += string;
+    }
+    return word;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    const string = [];
+    for (let i = word.length - 1; i >= 0; i--) {
+        string.push(word.charAt(i));
+    }
+    let finalWord = "";
+    for (let i = 0; i < string.length; i++) {
+        finalWord += string[i];
+    }
+    return (finalWord);
 };
 
 // Do not edit below this line
