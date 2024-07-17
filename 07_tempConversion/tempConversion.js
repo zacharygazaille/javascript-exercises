162e2f9682ce6b7fde50543fb2f4359cba2a0050
Zachary Gazaille <zacharygazaille@gmail.com>
2024-07-16 20:24:51 -0400

    Complete exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let newTemp = (temp - 32) * 5 / 9;
+  newTemp *= 10;
+  newTemp = Math.round(newTemp);
+  newTemp /= 10;
+  return newTemp;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let newTemp = temp * 9 / 5 + 32;
+  newTemp *= 10;
+  newTemp = Math.round(newTemp);
+  newTemp /= 10;
+  return newTemp;
 };
 
 // Do not edit below this line
